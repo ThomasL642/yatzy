6b1eff94c3ec309adcd33c41257b38f6537411cc
ThomasL642 <ThomasLawrence642@gmail.com>
2024-10-31 23:25:34 -0400

    sum and Bonus Displays

--- ScoreBoardButtons.js
+++ ScoreBoardButtons.js
@@ -10,23 +10,30 @@ function ScoreBoardButtons(category) {
     }else{
         const currentScoreboard = PlayerTurn === 1 ? scoreboardPlayer1 : scoreboardPlayer2;
         const yatzyChecker = new YatzyChecker(rollHistory.getHistory());
-        console.log("row " + category + " has been clicked");
         // calucate score + add score to score broad object
         score = yatzyChecker[category]();
         const shorthand = category.slice(5);
-        console.log("Score for " + shorthand + " is " + score);
         currentScoreboard.addScore(shorthand, score);
         // add score to html score broad
         const cellId = shorthand + PlayerTurn;
-        console.log(cellId);
         const cell = document.querySelector(`[data-cell="${cellId}"]`);
         cell.textContent = score;
-        // update total score
+        // update total score, sum and bonus
         const totalScore = currentScoreboard.calculateTotalScore();
-        console.log(totalScore);
-        const TotalcellId = "Total" + PlayerTurn;
-        const Totalcell = document.querySelector(`[data-cell="${TotalcellId}"]`);
-        Totalcell.textContent = totalScore;
+        const totalCellId = "Total" + PlayerTurn;
+        const totalCell = document.querySelector(`[data-cell="${totalCellId}"]`);
+        totalCell.textContent = totalScore;
+
+        const upperSum = currentScoreboard.calculateUpperSectionTotal();
+        const sumCellId = "Sum" + PlayerTurn;
+        const sumCell = document.querySelector(`[data-cell="${sumCellId}"]`);
+        sumCell.textContent = upperSum;
+
+        if (upperSum >= 63) {
+            const bonusCellId = "Bonus" + PlayerTurn;
+            const bonusCell = document.querySelector(`[data-cell="${bonusCellId}"]`);
+            bonusCell.textContent = 35;
+        }
 
         endTurn();
     }
